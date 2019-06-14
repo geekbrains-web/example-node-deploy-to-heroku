20180c9fae72f41638fcf4c35247acf478756f29
Oleg Shikov <ol.shikov@yandex.ru>
2019-06-14 13:26:50 +0300

    Change description in README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# example-node-deploy-to-heroku
-Пример деплоя приложения на Node.js на Heroku
+# Пример деплоя приложения на Node.js на сервера Heroku
+
+Демонстрация этого приложения: https://example-node-deploy.herokuapp.com/
+
+### Кратокое описание 
+Мы можем настоить деплой приложения либо через веб-интерфейс [Heroku](https://heroku.com), либо через консольную утилиту [Herocu CLI](https://devcenter.heroku.com/articles/heroku-cli#download-and-install).
+В `./Procfile` мы указываем, какую команду выполнить на сервере по окончанию деплоя. Это может быть `web: node server.js` либо `web: npm start`. Более подробное описание [Procfile](https://devcenter.heroku.com/articles/procfile).
+Есть возможность как настроить автоматический деплой при внесении изменений например в ветку master, так и деплой по CLI команде/нажатию кнопки в веб интерфейсе.
+
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const http = require('http');
 const port = process.env.PORT || 5000;
 
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server on ' + request.url)
+    const date = new Date();
+    console.log(request.url);
+    response.write('It\'s a simple Node.js server which was deployed to Heroku\n');
+    response.write(`Now is ${date.getDate()}.${(date.getMonth() + 1)}.${date.getFullYear()}, ${date.getHours()}:${date.getSeconds()} \n`);
+    response.end('You\'re on ' + request.url);
 }
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.error('something bad happened', err);
     }
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`);
 })
